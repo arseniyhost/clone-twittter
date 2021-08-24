c50a39ac0934ee32154b6a26c5905fff90eb36e5
arseniyhost <46376590+arseniyhost@users.noreply.github.com>
2021-08-24 21:33:06 +0300

    completed lesson3

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,8 +1,8 @@
 // @ts-nocheck
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
-export const theme = createMuiTheme({
+export const theme = createTheme({
     typography: {
         fontFamily: [
             'system-ui',
